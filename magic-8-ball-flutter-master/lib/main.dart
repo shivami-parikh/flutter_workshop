c4dac039887aac2aac70abc9c46d084df7a23b42
shivami-parikh <shah.atease@gmail.com>
2021-09-06 22:29:31 +0530

    day 3

--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -1,12 +1,41 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Magic 8 ball"),
-          ),
-          body: SafeArea(),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Magic 8 ball"),
+          backgroundColor: Colors.deepOrange,
         ),
+        body: MagicBall(),
+        backgroundColor: Colors.orange,
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballImageNumber = 1;
+
+  void changeBall() => setState(() {
+        ballImageNumber = Random().nextInt(5) + 1;
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        child: Image.asset("images/ball$ballImageNumber.png"),
+        onTap: changeBall,
       ),
     );
+  }
+}
